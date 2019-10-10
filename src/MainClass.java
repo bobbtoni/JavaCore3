e8250bd38634779c5bcc11a1d4de7eca3dff9b72
bobbtoni <bobb.toni.phone@gmail.com>
2019-10-10 10:33:27 +0300

    Домашнее задание 5. Многопоточность 2. Готовый код.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,16 +1,31 @@
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT/2), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+        final CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
+        Lock lock = new ReentrantLock();
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT+1);
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 100), cyclicBarrier, countDownLatch);
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+
+        try {
+            for (int i = 0; i < cars.length; i++) {
+                new Thread(cars[i]).start();
+            }
+            cyclicBarrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            countDownLatch.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
